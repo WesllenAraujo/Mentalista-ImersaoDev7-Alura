57678d31199719ce5564a157bd93f053afbd1694
Wesllen Araújo <wesllenaraujocarmo2007@gmail.com>
2023-11-26 12:59:36 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,18 @@
 var numeroSecreto = parseInt(Math.random() * 1001);
-var tentativas = 0;
-
+var tentativas = 1;
 while (chute != numeroSecreto) {
     var chute = prompt('Digite um número entre 0 e 1000');
-    //se o chute for igual ao número secreto
-    if (chute == numeroSecreto) {
-        alert(`Acertou em ${tentativas} tentivas!`)
-    } else if (chute > numeroSecreto) {
-        tentativas++;
-        alert(`Errou... o número secreto é menor. Você já errou ${tentativas} vezes!`);
-    } else if (chute < numeroSecreto) {
-        tentativas++;
-        alert(`Errou... o número secreto é maior. Você já errou ${tentativas} vezes!`);
-    }
+        if (chute == numeroSecreto) {
+            if (tentativas === 1) {
+                alert(`Acertou em ${tentativas} tentativa!`)
+            } else {
+                alert(`Acertou em ${tentativas} tentivas!`)
+            }
+        } else if (chute > numeroSecreto) {
+            tentativas++;
+            alert(`Errou... o número secreto é menor. Você já errou ${tentativas} vezes!`);
+        } else if (chute < numeroSecreto) {
+            tentativas++;
+            alert(`Errou... o número secreto é maior. Você já errou ${tentativas} vezes!`);
+        }
 }
